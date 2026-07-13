eb0ecb0a12a34f2b932a28b989fe680f38324888
JustDaDoge <henryjiang2022@gmail.com>
2026-07-13 17:50:42 -0400

    Updated evaluate.py
    Made it more consistent to trainer.py by adding tqdm stuff & made it so in case the last batch is less than the batch size it still calculates the loss correctly(since the loss will always divide by the batch size even if the last batch which may be less than batch_size). Also actually made it display loss.

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,5 +1,7 @@
 import torch
 from dataset import build_dataloaders
+from nn.losses import get_loss
+from tqdm.auto import tqdm
 
 def get_test_loader(batch_size):
     _, _, test_loader = build_dataloaders("embeddings/prompts.npz", batch_size=batch_size)
@@ -10,11 +12,30 @@ def evaluate(model, test_loader):
     model.eval()
     correct = 0
     total = 0
+    total_loss = 0.0
+    criterion = get_loss("cross_entropy")
 
+    progress = tqdm(
+        test_loader,
+        desc="Evaluation",
+        leave=False
+    )
     with torch.no_grad():
-        for input, label in test_loader:
-            outputs = model(input)
-            _, predicted = torch.max(outputs, 1)
+        for input, label in progress:
+            logits = model(input)
+
+            loss = criterion(logits, label)
+            total_loss += loss.item() * label.size(0) #in case the last_batch is less than 32 because drop_last=False by default
+
+            predicted = logits.argmax(dim=1)
             total += label.size(0)
             correct += (predicted == label).sum().item()
-    return correct/total
+
+            progress.set_postfix(
+                loss=f"{loss.item():.4f}",
+                acc=f"{correct / total:.3f}"
+            )
+    return (
+        total_loss / total,
+        correct / total
+    )
